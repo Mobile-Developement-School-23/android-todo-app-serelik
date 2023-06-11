fa2809dbadb494156b3c0ce7ddc6d72cb13bd3a0
tastyelik <zdivanal@gmail.com>
2023-06-12 00:44:08 +0200

    Add CoordinatorLayout

--- app/src/main/java/com/serelik/todoapp/list/TodoListFragment.kt
+++ app/src/main/java/com/serelik/todoapp/list/TodoListFragment.kt
@@ -1,6 +1,7 @@
 package com.serelik.todoapp.list
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -25,7 +26,24 @@ class TodoListFragment : Fragment(R.layout.fragment_todo_list) {
         recyclerView.adapter = todoItemAdapter
 
         todoItemAdapter.submitList(TodoItemsRepository.getItems())
+        getVisibilityTodoItemButton()?.setOnMenuItemClickListener {
+            updateVisibilityTodoDoneItemsStatus(true) // TODO
+            true
+        }
 
 
+    }
+
+    private fun getVisibilityTodoItemButton(): MenuItem? {
+        return viewBinding.toolbar.menu.findItem(R.id.action_hide)
+    }
+
+    private fun updateVisibilityTodoDoneItemsStatus(isDoneVisible: Boolean) {
+        val notificationIcon = if (isDoneVisible) {
+            R.drawable.ic_visibility_off
+        } else {
+            R.drawable.ic_visibility_on
+        }
+        getVisibilityTodoItemButton()?.setIcon(notificationIcon)
     }
 }
--- app/src/main/java/com/serelik/todoapp/repository/TodoItemsRepository.kt
+++ app/src/main/java/com/serelik/todoapp/repository/TodoItemsRepository.kt
@@ -32,6 +32,84 @@ object TodoItemsRepository {
             text = "Fourth todo",
             importance = TodoItemImportance.HIGH,
             created = LocalDate.of(2071, 8, 20)
-        )
+        ),
+        TodoItem(
+            id = "1",
+            text = "First todo",
+            created = LocalDate.now()
+        ),
+        TodoItem(
+            id = "2",
+            text = "Second todo",
+            deadline = LocalDate.of(2001, 8, 22),
+            importance = TodoItemImportance.LOW,
+            created = LocalDate.of(2001, 8, 20)
+        ),
+        TodoItem(
+            id = "3",
+            text = "Certain but she but shyness why cottage. Gay the put instrument sir entreaties affronting. Pretended exquisite see cordially the you. Weeks quiet do vexed or whose. Motionless if no to affronting imprudence no precaution. My indulged as disposal strongly attended. Parlors men express had private village man. Discovery moonlight recommend all one not. Indulged to answered prospect it bachelor is he bringing shutters. Pronounce forfeited mr direction oh he dashwoods ye unwilling.",
+            importance = TodoItemImportance.HIGH,
+            isDone = true,
+            created = LocalDate.of(2050, 6, 2),
+            deadline = LocalDate.of(2051, 8, 22),
+        ),
+        TodoItem(
+            id = "4",
+            text = "Fourth todo",
+            importance = TodoItemImportance.HIGH,
+            created = LocalDate.of(2071, 8, 20)
+        ),
+        TodoItem(
+            id = "1",
+            text = "First todo",
+            created = LocalDate.now()
+        ),
+        TodoItem(
+            id = "2",
+            text = "Second todo",
+            deadline = LocalDate.of(2001, 8, 22),
+            importance = TodoItemImportance.LOW,
+            created = LocalDate.of(2001, 8, 20)
+        ),
+        TodoItem(
+            id = "3",
+            text = "Certain but she but shyness why cottage. Gay the put instrument sir entreaties affronting. Pretended exquisite see cordially the you. Weeks quiet do vexed or whose. Motionless if no to affronting imprudence no precaution. My indulged as disposal strongly attended. Parlors men express had private village man. Discovery moonlight recommend all one not. Indulged to answered prospect it bachelor is he bringing shutters. Pronounce forfeited mr direction oh he dashwoods ye unwilling.",
+            importance = TodoItemImportance.HIGH,
+            isDone = true,
+            created = LocalDate.of(2050, 6, 2),
+            deadline = LocalDate.of(2051, 8, 22),
+        ),
+        TodoItem(
+            id = "4",
+            text = "Fourth todo",
+            importance = TodoItemImportance.HIGH,
+            created = LocalDate.of(2071, 8, 20)
+        ),
+        TodoItem(
+            id = "1",
+            text = "First todo",
+            created = LocalDate.now()
+        ),
+        TodoItem(
+            id = "2",
+            text = "Second todo",
+            deadline = LocalDate.of(2001, 8, 22),
+            importance = TodoItemImportance.LOW,
+            created = LocalDate.of(2001, 8, 20)
+        ),
+        TodoItem(
+            id = "3",
+            text = "Certain but she but shyness why cottage. Gay the put instrument sir entreaties affronting. Pretended exquisite see cordially the you. Weeks quiet do vexed or whose. Motionless if no to affronting imprudence no precaution. My indulged as disposal strongly attended. Parlors men express had private village man. Discovery moonlight recommend all one not. Indulged to answered prospect it bachelor is he bringing shutters. Pronounce forfeited mr direction oh he dashwoods ye unwilling.",
+            importance = TodoItemImportance.HIGH,
+            isDone = true,
+            created = LocalDate.of(2050, 6, 2),
+            deadline = LocalDate.of(2051, 8, 22),
+        ),
+        TodoItem(
+            id = "4",
+            text = "Fourth todo",
+            importance = TodoItemImportance.HIGH,
+            created = LocalDate.of(2071, 8, 20)
+        ),
     )
 }
