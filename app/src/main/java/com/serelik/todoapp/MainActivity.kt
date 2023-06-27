055afd89f03aea8ee077c25bbe7fd5aabd36f0c9
tastyelik <zdivanal@gmail.com>
2023-06-27 20:09:35 +0200

    Add authorization with yandex id

--- app/src/main/java/com/serelik/todoapp/MainActivity.kt
+++ app/src/main/java/com/serelik/todoapp/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.serelik.todoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.serelik.todoapp.authorizationFragment.AuthorizationFragment
+import com.serelik.todoapp.data.local.TokenStorage
 import com.serelik.todoapp.list.TodoListFragment
 
 class MainActivity : AppCompatActivity() {
+    private val tokenStorage by lazy { TokenStorage() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (savedInstanceState == null) {
+
+            val fragment = if (tokenStorage.hasToken())
+                TodoListFragment()
+            else
+                AuthorizationFragment()
+
             supportFragmentManager
                 .beginTransaction()
-                .add(android.R.id.content, TodoListFragment())
+                .add(android.R.id.content, fragment)
                 .commit()
         }
     }
---
+++ app/src/main/java/com/serelik/todoapp/data/local/TokenStorage.kt
@@ -0,0 +1,32 @@
+package com.serelik.todoapp.data.local
+
+import android.content.Context
+import com.serelik.todoapp.TodoApp
+
+class TokenStorage {
+
+    private val sharedPref = TodoApp.context.getSharedPreferences(
+        SHARED_PREF_KEY, Context.MODE_PRIVATE
+    )
+
+    fun saveToken(token: String) {
+        with(sharedPref.edit()) {
+            putString(TOKEN_KEY, token)
+            apply()
+        }
+    }
+
+    fun getToken(): String? {
+        return sharedPref.getString(TOKEN_KEY, null)
+    }
+
+    fun hasToken(): Boolean {
+        return sharedPref.contains(TOKEN_KEY)
+    }
+
+    companion object {
+        private const val TOKEN_KEY = "TOKEN_KEY"
+        private const val SHARED_PREF_KEY = "SHARED_PREF_KEY"
+    }
+
+}
