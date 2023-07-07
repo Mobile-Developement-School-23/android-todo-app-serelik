aafbe1d89fe9af635ad47d724537ea47fef6951d
tastyelik <zdivanal@gmail.com>
2023-07-07 16:06:40 +0200

    add SwipeHelperCallback

--- app/src/main/java/com/serelik/todoapp/list/TodoListFragment.kt
+++ app/src/main/java/com/serelik/todoapp/list/TodoListFragment.kt
@@ -1,23 +1,15 @@
 package com.serelik.todoapp.list
 
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Rect
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.util.TypedValue
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
-import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.serelik.todoapp.MainActivity
 import com.serelik.todoapp.R
@@ -26,15 +18,14 @@ import com.serelik.todoapp.di.TodoListFragmentComponent
 import com.serelik.todoapp.edit.TodoEditFragment
 import java.io.IOException
 import javax.inject.Inject
-import kotlin.math.roundToInt
 
 class TodoListFragment : Fragment(R.layout.fragment_todo_list) {
 
-    private val viewModel: TodoListViewModel by viewModels() {
+    private val viewModel: TodoListViewModel by viewModels {
         viewModelFactory
     }
 
-    lateinit var component: TodoListFragmentComponent
+    private lateinit var component: TodoListFragmentComponent
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -50,6 +41,18 @@ class TodoListFragment : Fragment(R.layout.fragment_todo_list) {
     private val viewBinding by viewBinding(FragmentTodoListBinding::bind)
 
     private val supportFragmentManager by lazy { requireActivity().supportFragmentManager }
+
+    private val onSwipeTodo = object : OnSwipeTodo {
+        override fun onSwipeDone(position: Int) {
+            val item = todoItemAdapter.getItemTodo(position) ?: return
+            viewModel.changedStateDone(item, !item.isDone)
+        }
+
+        override fun onSwipeDelete(position: Int) {
+            val item = todoItemAdapter.getItemTodo(position) ?: return
+            viewModel.remove(item.id)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -126,110 +129,12 @@ class TodoListFragment : Fragment(R.layout.fragment_todo_list) {
             .commit()
     }
 
+
     private fun swipeFunctionality() {
-        val displayMetrics: DisplayMetrics = resources.displayMetrics
-        val width = (displayMetrics.widthPixels / displayMetrics.density).toInt().dp
 
-        val swipeDeleteIcon =
-            ResourcesCompat.getDrawable(resources, R.drawable.ic_swipe_delete, null)
-        val swipeCheckedIcon =
-            ResourcesCompat.getDrawable(resources, R.drawable.ic_swipe_checked, null)
 
-        val deleteColor = ContextCompat.getColor(requireContext(), R.color.red)
-        val checkedDoneColor = ContextCompat.getColor(requireContext(), R.color.green)
-
-        val swipeHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
-            0,
-            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-        ) {
-            val rect = Rect().apply {
-                left = 0
-                right = width
-            }
-            val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ) = false
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                if (viewHolder !is TodoItemViewHolder) {
-                    return
-                }
-
-                val pos = viewHolder.adapterPosition
-                val item = todoItemAdapter.getItemTodo(pos) ?: return
-
-                if (direction == ItemTouchHelper.LEFT) {
-                    viewModel.remove(item.id)
-                } else {
-                    viewModel.changedStateDone(item, !item.isDone)
-                }
-            }
-
-            override fun onChildDraw(
-                canvas: Canvas,
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                dX: Float,
-                dY: Float,
-                actionState: Int,
-                isCurrentlyActive: Boolean
-            ) {
-                if (viewHolder !is TodoItemViewHolder) {
-                    return
-                }
-
-                paint.color = when {
-                    dX < 0 -> deleteColor
-                    else -> checkedDoneColor
-                }
-
-                rect.apply {
-                    top = viewHolder.itemView.top
-                    bottom = viewHolder.itemView.bottom
-                }
-
-                canvas.drawRect(rect, paint)
-
-                val iconMargin = resources.getDimension(R.dimen.DeleteDrawableMargin)
-                    .roundToInt()
-                val drawableHeight = swipeDeleteIcon?.intrinsicHeight ?: 0
-                val verticalPadding = (viewHolder.itemView.height - drawableHeight) / 2
-                if (swipeCheckedIcon != null) {
-                    swipeCheckedIcon.bounds = Rect(
-                        iconMargin,
-                        viewHolder.itemView.top + verticalPadding,
-                        iconMargin + swipeCheckedIcon.intrinsicWidth,
-                        viewHolder.itemView.top + swipeCheckedIcon.intrinsicHeight + verticalPadding
-                    )
-                }
-                if (swipeDeleteIcon != null) {
-                    swipeDeleteIcon.bounds = Rect(
-                        width - iconMargin * 2 - swipeDeleteIcon.intrinsicWidth,
-                        viewHolder.itemView.top + verticalPadding,
-                        width - iconMargin * 2,
-                        viewHolder.itemView.top + swipeDeleteIcon.intrinsicHeight +
-                                verticalPadding
-                    )
-                }
-
-                if (dX < 0) swipeDeleteIcon?.draw(canvas) else swipeCheckedIcon?.draw(canvas)
-
-                super.onChildDraw(
-                    canvas,
-                    recyclerView,
-                    viewHolder,
-                    dX,
-                    dY,
-                    actionState,
-                    isCurrentlyActive
-                )
-            }
-        })
-
+        val swipeHelperCallback = SwipeHelperCallback(requireContext(), onSwipeTodo)
+        val swipeHelper = ItemTouchHelper(swipeHelperCallback)
         swipeHelper.attachToRecyclerView(viewBinding.recyclerView)
     }
 
@@ -254,10 +159,5 @@ class TodoListFragment : Fragment(R.layout.fragment_todo_list) {
         viewBinding.swipeRefreshLayout.isRefreshing = false
     }
 
-    private val Int.dp
-        get() = TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            toFloat(),
-            resources.displayMetrics
-        ).roundToInt()
+
 }
